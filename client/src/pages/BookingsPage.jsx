700757c03e222db2c62ec6478a880f9b65bb5603
A1n2F <alissonfferreira22@gmail.com>
2025-10-21 09:35:42 -0300

    Single booking page

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -3,8 +3,8 @@ import { useState } from "react"
 import { AccountNav } from "../components/AccountNav"
 import { PlaceImg } from "../components/PlaceImg"
 import axios from "axios"
-import { differenceInCalendarDays, format } from "date-fns"
 import { Link } from "react-router-dom"
+import { BookingDates } from "../components/BookingDates"
 
 export const BookingsPage = () => {
     const [bookings, setBookings] = useState([])
@@ -20,7 +20,7 @@ export const BookingsPage = () => {
 
             <div className="max-w-5xl mx-auto">
                 {bookings?.length > 0 && bookings.map(booking => (
-                    <div className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden">
+                    <Link to={`/account/bookings/${booking._id}`} className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden">
                         <div className="w-48">
                             <PlaceImg place={booking.place} />
                         </div>
@@ -29,31 +29,7 @@ export const BookingsPage = () => {
                             <h2 className="text-xl">{booking.place.title}</h2>
 
                             <div className="text-xl">
-                                <div className="flex gap-1 mb-2 mt-4">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                                    </svg>
-
-                                    {differenceInCalendarDays(new Date(booking.checkOut), new Date(booking.checkIn))} nights:
-
-                                    <div className="flex gap-1 items-center ml-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
-                                        </svg>
-
-                                        {format(new Date(booking.checkIn), "dd/MM/yyyy")}
-                                    </div>
-
-                                    &rarr;
-
-                                    <div className="flex gap-1 items-center">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
-                                        </svg>
-
-                                        {format(new Date(booking.checkOut), "dd/MM/yyyy")}
-                                    </div>
-                                </div>
+                                <BookingDates booking={booking} className="mb-2 mt-4" />
 
                                 <div className="flex gap-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
@@ -64,7 +40,7 @@ export const BookingsPage = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </div>
